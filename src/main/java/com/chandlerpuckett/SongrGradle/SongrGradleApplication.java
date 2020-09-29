44f9c6f8c41ee6b11b9c386862af1e2ce389bf1e
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-29 09:58:33 -0700

    first commit

---
+++ src/main/java/com/chandlerpuckett/SongrGradle/SongrGradleApplication.java
@@ -0,0 +1,13 @@
+package com.chandlerpuckett.SongrGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandlerpuckett/SongrGradle/SongrGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandlerpuckett.SongrGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
